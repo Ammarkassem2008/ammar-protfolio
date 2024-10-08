50c12cc8406503da8a8887779e7a2acbd52f86f7
Ammarkassem2008 <ammarkassem215@gmail.com>
2024-10-08 20:08:27 +0300

    update data.js

--- js/data.js
+++ js/data.js
@@ -1,49 +1,49 @@
 const data = [
   {
     id: 1,
-    img: '/img/project1.png',
+    img: '../img/project1.png',
     text: 'Dash board',
   },
   {
     id: 2,
-    img: '/img/project2.png',
+    img: '../img/project2.png',
     text: 'weather App',
   },
   {
     id: 3,
-    img: '/img/project3.png',
+    img: '../img/project3.png',
     text: 'Remove bg',
 },
   {
     id: 4,
-    img: '/img/project4.png',
+    img: '../img/project4.png',
     text: 'Busniess Website',
 },
 
   {
     id: 5,
-    img: '/img/project6.png',
+    img: '../img/project6.png',
     text: 'To-Do List',
 },
   {
     id: 6,
-    img: '/img/project7.png',
+    img: '../img/project7.png',
     text: 'E-commers on salla.sa',
   },
   {
     id: 7,
-    img: '/img/project8.png',
+    img: '../img/project8.png',
     text: 'E-commers on salla.sa',
   },
   
    {
     id: 8,
-    img: '/img/project9.png',
+    img: '../img/project9.png',
     text: 'E-commers on salla.sa',
   },
   {
     id: 9,
-    img: '/img/project10.png',
+    img: '../img/project10.png',
     text: 'E-commers on salla.sa',
   },  
 ]
